12cc3991b97231053189c9f191001d061050d6c2
Adriano Raiano <adriano@raiano.ch>
2022-02-11 20:59:52 +0100

    i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
+import { Component, Suspense } from 'react';
+import { useTranslation, withTranslation, WithTranslation, Trans } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// use hoc for class based components
+class LegacyWelcomeClass extends Component<WithTranslation> {
+  render() {
+    const { t } = this.props;
+    return <h2>{t('title')}</h2>;
+  }
+}
+const Welcome = withTranslation()(LegacyWelcomeClass);
+
+// Component using the Trans component
+function MyComponent() {
+  return (
+    <Trans i18nKey="description.part1">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </Trans>
+  );
+}
+
+// page uses the hook
+function Page() {
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Welcome />
+        <button type="button" onClick={() => i18n.changeLanguage('de')}>
+          de
+        </button>
+        <button type="button" onClick={() => i18n.changeLanguage('en')}>
+          en
+        </button>
       </header>
+      <div className="App-intro">
+        <MyComponent />
+      </div>
+      <div>{t('description.part2')}</div>
     </div>
   );
 }
 
-export default App;
+// loading component for suspense fallback
+const Loader = () => (
+  <div className="App">
+    <img src={logo} className="App-logo" alt="logo" />
+    <div>loading...</div>
+  </div>
+);
+
+// here app catches the suspense from page in case translations are not yet loaded
+export default function App() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Page />
+    </Suspense>
+  );
+}
